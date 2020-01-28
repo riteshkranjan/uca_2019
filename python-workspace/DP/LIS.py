23f700ee549965ac3472d77ab47f7578018b4df5
riteshkranjan <riteshk.ranjan@gmail.com>
2020-01-28 12:45:09 +0530

    Update LIS.py

--- python-workspace/DP/LIS.py
+++ python-workspace/DP/LIS.py
@@ -1,12 +1,18 @@
-def Lis(a, n):
-    if n == 0:
-        return 1, a[n]
-    c, m = Lis(a, n-1)
-    if m < a[n]:
-        return 1+c, a[n]
-    return c,m
+def lis(a, n):
+    LIS = [1]*n
+    for i in range(1,n):
+        max = 1
+        for j in range(i):
+            if a[j] < a[i] and max < 1 + LIS[j]:
+                max  = 1 + LIS[j]
+        LIS[i] = max
+    max = -1
+    for i in LIS:
+        if max < i:
+            max = i
+    return max
 
 if __name__ == "__main__":
-    a = [10, 22, 9, 33]
-    c,m = Lis(a,len(a)-1)
-    print(c)
+    a = [10, 22, 9, 33,21,50,41,60,20]
+    n = len(a)
+    print("longest increasing squence in arr {} is {}".format(a,lis(a,n)))
